e5ac79f16a783bd099c7064e201e786e19bca76a
Laclac <ngochths150417@fpt.eedu.vn>
2023-01-11 14:34:47 +0700

    commit 1

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'txxe2nval9'
+const apiId = 'thiju18t8k'
 export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-1.amazonaws.com/dev`
 
 export const authConfig = {
